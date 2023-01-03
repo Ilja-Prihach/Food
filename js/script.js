0a3ce491b61f0763d036080852f38de7b5b00087
Ilja-Prihach <piratehnik9508@gmail.com>
2023-01-03 15:39:05 +0300

    fetch api

--- js/script.js
+++ js/script.js
@@ -273,29 +273,32 @@ window.addEventListener('DOMContentLoaded', () => {     //назначаем г�
                 margin: 0 auto;
             `;
             form.insertAdjacentElement('afterend', statusMessage);
-         
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
             const formData = new FormData(form);
- 
+
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-            const json = JSON.stringify(object);
- 
-            request.send(json);
- 
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal (message.failure);
-                }
+
+
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                form.reset();
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal (message.failure);
+            }).finally(() => {
+                form.reset();
             });
         });
     }
@@ -325,5 +328,16 @@ window.addEventListener('DOMContentLoaded', () => {     //назначаем г�
         }, 4000);
     }
 
+
+    // Fetch API
+    // fetch('https://jsonplaceholder.typicode.com/posts', {
+    //     method: "POST",
+    //     body: JSON.stringify({name: 'Alex'}),
+    //     headers: {
+    //         'Counter-type': 'application/json'
+    //     }
+    // })
+    // .then(response => response.json())
+    // .then(json => console.log(json));
 
 });
